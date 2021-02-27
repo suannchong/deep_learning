87d4ec779f8e4c98785e294704b3181ed62154c4
Su-Ann Chong <schong3@vols.utk.edu>
2021-02-27 16:39:58 -0500

    initial commit

---
+++ proj2/code/parameters.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+#Generate data and weights for "example2"
+def generateExample2():
+    # this example has an input of 7x7, one 3x3 convolution layer
+    # with two kernels, another 3x3 convolution layer with a single kernel
+    # a flatten layer and a single neuron for the output
+
+    #Set a seed (that way you get the same values when rerunning the function)
+    np.random.seed(10)
+
+    #First hidden layer, two kernels
+    l1k1=np.random.rand(3,3)
+    l1k2=np.random.rand(3,3)
+    l1b1=np.random.rand(1)
+    l1b2=np.random.rand(1)
+
+    # first layer: 5x5x2=50 neurons, (3x3+1)x2=20 weights 
+
+    #second hidden layer, one kernel, two channels
+    l2c1=np.random.rand(3,3)
+    l2c2=np.random.rand(3,3)
+    l2b=np.random.rand(1)
+
+    # second layer: 3x3x1=9 neurons, (3x3x2+1)x1=19 weights 
+
+    #output layer, fully connected
+    l3=np.random.rand(1,9)
+    l3b=np.random.rand(1)
+
+    # output layer: 1 neuron, 10 weights 
+
+    #input and output
+    input=np.random.rand(7,7)
+    output=np.random.rand(1)
+
+    return l1k1,l1k2,l1b1,l1b2,l2c1,l2c2,l2b,l3,l3b,input,output
+
+def generateExample1():
+    # this example has a 5x5 input, one 3x3 convolution layer
+    # with a single kernel, a flatten layer, 
+    # and a single neuron for the output
+
+    # set a seed 
+    np.random.seed(42)
+
+    # 1 hidden layer, 1 kernel
+    l1  = np.random.rand(3,3)
+    l1b = np.random.rand(1)
+
+    # first layer: 3x3x1=9 neurons, (3x3x1+1)x1=10 weights
+
+    # output layer, fully connected
+    l2  = np.random.rand(1,9)
+    l2b = np.random.rand(1)
+
+    # output layer: 1 neuron, 10 weights 
+
+    # input and output
+    input = np.random.rand(5,5)
+    output = np.random.rand(1)
+
+    return l1, l1b, l2, l2b, input, output 
+
+def generateExample3():
+    # this example has a 8x8 input, one 3x3 convolution layer
+    # with two kernels, a 2x2 max pooling layer, a flatten layer,
+    # a single neuron for the output
+
+    # set a seed
+    np.random.seed(42)
+
+    # 1 hidden layer, 2 kernels
+    l1k1 = np.random.rand(3,3)
+    l1k2 = np.random.rand(3,3)
+    l1b1 = np.random.rand(1)
+    l1b2 = np.random.rand(1)
+
+    # first layer: 6x6x2=72 neurons, (3x3x1+1)x2=20 weights 
+    # pooling layer: 3x3x2 neurons
+
+    # output layer, fully connected
+    l2   = np.random.rand(1,18)
+    l2b  = np.random.rand(1)
+
+    # output layer: 1 neuron, 72 weights 
+
+    # input and output
+    input = np.random.rand(8,8)
+    output = np.random.rand(1)
+
+    return l1k1, l1k2, l1b1, l1b2, l2, l2b, input, output 
+
+
---
+++ proj2/code/tensorflowtest_example1.py
@@ -0,0 +1,64 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from parameters import *
+
+# -------------------------------------------------------------------------------
+# 							EXAMPLE 1
+# -------------------------------------------------------------------------------
+
+# this example has a 5x5 input, one 3x3 convolution layer with a single kernel, 
+# a flatten layer, and a single neuron for the output
+
+# create a feedforward network
+model=Sequential()
+
+# Add convolutional layers, flatten, and fully connected layer
+# arg for Conv2D: num_kernels, kernel_size, 
+# input_shape (input_size, input_size, num_channels)
+model.add(layers.Conv2D(1,3,input_shape=(5,5,1), activation='sigmoid'))
+model.add(layers.Flatten())
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# call weight/data generating function
+l1, l1b, l2, l2b, input, output = generateExample1()
+
+# set weights to desired values
+
+# first layer weights and bias
+l1=l1.reshape(3,3,1,1)
+model.layers[0].set_weights([l1,l1b])
+
+# output layer 
+model.layers[2].set_weights([np.transpose(l2),l2b])
+
+# setting inputs
+img=np.expand_dims(input,axis=(0,3))
+
+#print needed values.
+np.set_printoptions(precision=5)
+print("-------------------------------------------")
+print("-------------------------------------------")
+print("		EXAMPLE 1                   ")
+print("-------------------------------------------")
+print("-------------------------------------------")
+
+print('model output before:')
+print(model.predict(img))
+sgd = optimizers.SGD(lr=100)
+model.compile(loss='MSE', optimizer=sgd, metrics=['accuracy'])
+history=model.fit(img,output,batch_size=1,epochs=1)
+print('model output after:')
+print(model.predict(img))
+
+print('1st convolutional layer, 1st kernel weights:')
+print(np.squeeze(model.get_weights()[0][:,:,0,0]))
+print('1st convolutional layer, 1st kernel bias:')
+print(np.squeeze(model.get_weights()[1][0]))
+
+print('fully connected layer weights:')
+print(np.squeeze(model.get_weights()[2]))
+print('fully connected layer bias:')
+print(np.squeeze(model.get_weights()[3][0]))
